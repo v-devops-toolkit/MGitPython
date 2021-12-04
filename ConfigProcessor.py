488c88d19943a2083d9e7c2a9959181c9da8e719
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2021-12-04 10:25:16 +0100

    refactor: extract ConfigProcessor class

---
+++ ConfigProcessor.py
@@ -0,0 +1,80 @@
+import subprocess
+import yaml
+import pprint
+from jinja2 import Environment, BaseLoader
+import os
+import sys
+
+class ConfigProcessor(object):
+
+    def __init__(self, dir: str = '.'):
+        self.__dir = dir
+        self.__config = {}
+        self.__config['repos'] = []
+        filename = os.path.join(dir, "mgit.yaml")
+        if (os.path.isfile(filename)):
+            self.load_config_file(filename)
+        else:
+            self.generate_config()
+
+    def set_config(self, config):
+        self.__config = config
+
+    def get_config(self):
+        return self.__config
+
+    def get_dir(self):
+        return self.__dir
+
+    def get_repos(self):
+        return self.__config['repos']
+
+    def load_raw_config_file(self, filename):
+        with open(filename, "r") as stream:
+            self.set_config(yaml.safe_load(stream))
+
+    def process_raw_config_file(self):
+        if (not self.__config.get('repos')):
+            self.__config['repos'] = []
+        if (not self.__config.get('baseUrl')):
+            self.__config['baseUrl'] = ''
+        if (not self.__config.get('dir')):
+            self.__config['dir'] = ''
+        if (self.__config['dir'] != ''):
+            self.__config['dir'] = self.__config['dir'].rstrip('/') + '/'
+        for item in self.__config['repos']:
+            if (item.get('dir') == None):
+                item['dir'] = self.__config['dir']
+            if (item.get('url') == None):
+                rtemplate = Environment(loader=BaseLoader).from_string(self.__config['baseUrl'])
+                item['url'] = rtemplate.render(item)
+            if (item.get('tags') == None):
+                item['tags'] = []
+            if (not 'all' in item['tags']):
+                item['tags'].append('all')
+
+    def load_config_file(self, filename):
+        self.load_raw_config_file(filename)
+        self.process_raw_config_file()
+
+    def generate_config(self):
+        self.__config['repos'] = []
+        for file in os.listdir(self.__dir):
+            current = os.path.join(self.__dir, file)
+            current_dot_git = os.path.join(current, '.git')
+            if (os.path.isdir(current) and os.path.isdir(current_dot_git)):
+                item = {}
+                item['name'] = file
+                item['dir'] = self.__dir
+                item['tags'] = ['all']
+                self.__config['repos'].append(item)
+        self.__config['repos'] = sorted(self.__config['repos'], key=lambda x: x['name'])
+        self.process_raw_config_file()
+
+    def get_config_yaml(self):
+        return yaml.dump(self.__config)
+
+    def save_config_as_yaml(self, filename):
+        with open(filename, "w") as text_file:
+            text_file.write(self.get_config_yaml())
+
--- MultiGit.py
+++ MultiGit.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python3
-
-import fire
 import subprocess
 import yaml
 import pprint
 from jinja2 import Environment, BaseLoader
 import os
 import sys
+import ConfigProcessor
 
 class MultiGit(object):
     """
@@ -14,65 +12,10 @@ class MultiGit(object):
     """
 
     def __init__(self, dir: str = '.'):
-        self.__dir = dir
-        self.__config = {}
-        self.__config['repos'] = []
-        filename = os.path.join(dir, "mgit.yaml")
-        if (os.path.isfile(filename)):
-            self.__load_config_file__(filename)
-        else:
-            self.__generate_config()
-
-    def __set_config__(self, config):
-        self.__config = config
-
-    def __get_config__(self):
-        return self.__config
-
-    def __load_raw_config_file__(self, filename):
-        with open(filename, "r") as stream:
-            self.__set_config__(yaml.safe_load(stream))
-
-    def __process_raw_config_file__(self):
-        if (not self.__config.get('repos')):
-            self.__config['repos'] = []
-        if (not self.__config.get('baseUrl')):
-            self.__config['baseUrl'] = ''
-        if (not self.__config.get('dir')):
-            self.__config['dir'] = ''
-        if (self.__config['dir'] != ''):
-            self.__config['dir'] = self.__config['dir'].rstrip('/') + '/'
-        for item in self.__config['repos']:
-            if (item.get('dir') == None):
-                item['dir'] = self.__config['dir']
-            if (item.get('url') == None):
-                rtemplate = Environment(loader=BaseLoader).from_string(self.__config['baseUrl'])
-                item['url'] = rtemplate.render(item)
-            if (item.get('tags') == None):
-                item['tags'] = []
-            if (not 'all' in item['tags']):
-                item['tags'].append('all')
-
-    def __load_config_file__(self, filename):
-        self.__load_raw_config_file__(filename)
-        self.__process_raw_config_file__()
-
-    def __generate_config(self):
-        self.__config['repos'] = []
-        for file in os.listdir(self.__dir):
-            current = os.path.join(self.__dir, file)
-            current_dot_git = os.path.join(current, '.git')
-            if (os.path.isdir(current) and os.path.isdir(current_dot_git)):
-                item = {}
-                item['name'] = file
-                item['dir'] = self.__dir
-                item['tags'] = ['all']
-                self.__config['repos'].append(item)
-        self.__config['repos'] = sorted(self.__config['repos'], key=lambda x: x['name'])
-        self.__process_raw_config_file__()
+        self.__configProcessor = ConfigProcessor.ConfigProcessor(dir)
 
     def __run_commands__(self, commands, tag: str = 'all'):
-        for item in self.__config['repos']:
+        for item in self.__configProcessor.get_repos():
             if (tag in item['tags']):
                 print("========================================")
                 print(f"REPO: {item['name']}")
@@ -83,20 +26,14 @@ class MultiGit(object):
         """
         debug command
         """
-        pprint.pprint(self.__config)
+        pprint.pprint(self.__configProcessor.get_config())
 
     def dump_config(self, filename: str = 'mgit.yaml'):
         """
         dump_config command
         """
-        self._save_config_as_yaml(os.path.join(self.__dir, filename))
-
-    def _get_config_yaml(self):
-        return yaml.dump(self.__config)
-
-    def _save_config_as_yaml(self, filename):
-        with open(filename, "w") as text_file:
-            text_file.write(self._get_config_yaml())
+        tmp_filename = os.path.join(self.__configProcessor.get_dir(), filename)
+        self.__configProcessor.save_config_as_yaml(tmp_filename)
 
     def version(self):
         """
@@ -192,9 +129,3 @@ class MultiGit(object):
                 print("========================================")
                 print(f"REPO: {item['name']}")
                 process = subprocess.run(["git", "clone", f"{item['url']}"], cwd=f"{item['dir']}")
-
-def main():
-    fire.Fire(MultiGit)
-
-if __name__ == '__main__':
-    main()
